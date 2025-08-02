4defe69bb5598bffc5ba573f0785cb5134d93104
Arpan050 <arpanpaul12345@gmail.com>
2025-08-02 22:47:21 +0530

    assignment 13

---
+++ assignment 13/id card/script.js
@@ -0,0 +1,5 @@
+function moreinfo() {
+  document.getElementById("College").innerText = "K.R. Mangalam University";
+  document.getElementById("Course").innerText = "B.Tech CSE";
+  document.getElementById("Semester").innerText = "7th";
+}
